edf12d0d28229b48660031319375da3ecf0e3401
Priyabrata Das <daspriyabrat10@gmail.com>
2026-05-28 19:49:27 +0530

    add comment model with schema definition

---
+++ src/models/comment.model.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        required: true,
+    },
+}, { timestamps: true });
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
