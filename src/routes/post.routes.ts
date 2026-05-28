20f49c270dbef872f4047a4cb5440f6aa06428d7
Priyabrata Das <daspriyabrat10@gmail.com>
2026-05-28 22:38:26 +0530

    add getFeed and unlikePost routes to post routes

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,13 +1,15 @@
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { Router } from "express";
-import { createPost, getAllPosts, getPostById, deletePost, likePost } from "../controllers/post.controllers";
+import { createPost, getAllPosts, getPostById, deletePost, likePost, getFeed, unlikePost } from "../controllers/post.controllers";
 
 const router = Router();
 
 router.post("/", authMiddleware, createPost);
+router.get("/feed", authMiddleware, getFeed);
 router.get("/", getAllPosts)
 router.get("/:id", getPostById)
 router.delete("/:id", authMiddleware, deletePost)
 router.post("/:id/like", authMiddleware, likePost)
+router.delete("/:id/like", authMiddleware, unlikePost);
 
 export default router;
